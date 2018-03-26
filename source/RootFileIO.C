5a64205ea9177ef31935184a558fa8f6893e507e
NIaa <tianbonian@gmail.com>
2018-03-26 09:42:32 +0800

    2.1
    Back up
    Todo Complex detector.

--- source/RootFileIO.C
+++ source/RootFileIO.C
@@ -22,25 +22,15 @@
 //
 //*****************************************************************************
 
-#if 0
-double the_cos_fit(double* vars, double* pars){
-  return pars[0]*cos(vars[0]*2+pars[1])+pars[2];
-}
-TF1 cos_fit("cos_fit",the_cos_fit,0.,3.14,3);
-#endif
-#if 1
 double the_sin2_fit(double *vars, double *pars){
   return pars[0]*pow(sin(vars[0]+pars[1]),2)+pars[2];
 }
 TF1 sin2_fit("sin2_fit",the_sin2_fit,0.,3.14,3);
-#endif
 TH1F h("h","PHI_FIT;Phi [rad];Entries [0.02]",157,0.,3.14);
 TH2F hxy("px-py","px-py;px [MeV/c];py [MeV/c]",100, -0.05, 0.05, 100, -0.05, 0.05);
 TH2F hxz("px-pz","px-pz;px [MeV/c];pz [MeV/c]",100, -0.05, 0.05, 100, -0.05, 0.05);
 TH2F hyz("py-pz","py-pz;py [MeV/c];pz [MeV/c]",100, -0.05, 0.05, 100, -0.05, 0.05);
 TH1F p2("momentum",";p [MeV/c];Entries [2.5*10^-4]",200,0.,0.05);
-
-// TH1F histo("histo","Signal plus background;X vals;Y Vals",50,0,20);
 
 void ReadRoot();
 
@@ -62,16 +52,9 @@ void ReadRoot()
   fTree1->SetBranchAddress("SimEvent", &fEvent);
 
 // set the parameters before the fit 
-#if 0
-  double A=150, delta=0, n=150;
-  cos_fit.SetParameters(A,delta,n);
-  cos_fit.SetParNames("Amplitude","#delta","n");
-#endif
-#if 1
   double A=1000, delta=0, n=0;
   sin2_fit.SetParameters(A,delta,n);
   sin2_fit.SetParNames("Amplitude","#delta","n");
-#endif
   int sz = fTree1->GetEntries();
   cout << "sz: " << sz << endl; //size
 
@@ -84,12 +67,9 @@ void ReadRoot()
     p2.Fill(sqrt(pow(fEvent->GetMomentum().x(),2)+pow(fEvent->GetMomentum().y(),2)+pow(fEvent->GetMomentum().z(),2)));
   }
   
-  // auto fitResPtr = h.Fit(&cos_fit, "S"); //quiet ="q"
   auto fitResPtr = h.Fit(&sin2_fit, "S"); //quiet ="q"
  
   fitResPtr->Print(); // print fit results
   
-  // Same DrawClone
-  // h.Draw();
   fRootFp->Close();
 }
---
+++ source/include/xxxxxxxxxx.hh
@@ -0,0 +1,7 @@
+
+#define SIZE      20
+#define _WORLD    0
+#define _DETECTOR 1
+#define _WINDOW   2
+#define _CERAMICS 3
+#define _SHELL    222
